e7e23499da6f77bf055e64f1cca1da9f598d3358
Naruemon061 <118896507+Naruemon061@users.noreply.github.com>
2022-12-21 18:07:49 +0700

    เรียน

---
+++ public_html/index.php
@@ -0,0 +1,16 @@
+# index.php 
+<h1>My PHP</h1>
+<?php
+/*<?php ?>*/
+//
+$myval = "Hello World";
+$myValuel = 0;
+echo $myval . "Welcome" . $myval;
+?>
+<!--<?php echo "<br>"; ?>-->
+<br>
+
+<?php
+{{ }}
+echo "this is my php {$myval}"
+?>
